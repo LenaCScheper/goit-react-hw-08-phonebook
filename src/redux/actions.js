7ca91ec647a3bd22c961aa4bde1c7e17d8da99ec
LenaCScheper <ven.lenysja@ukr.net>
2022-05-06 18:12:17 +0300

    добавила стартовые данные и скрипт

---
+++ src/redux/actions.js
@@ -0,0 +1,17 @@
+// import axios from 'axios';
+import { createAction } from '@reduxjs/toolkit';
+// import { addContact } from '../services/contact-api';
+
+export const addContactRequest = createAction('contacts/addContactRequest');
+export const addContactSuccess = createAction('contacts/addContactSuccsess');
+export const addContactError = createAction('contacts/addContactError');
+
+export const deleteContactRequest = createAction('contacts/deleteContactRequest');
+export const deleteContactSuccess = createAction('contacts/deleteContactSuccsess');
+export const deleteContactError = createAction('contacts/deleteContactError');
+
+export const fetchContactRequest = createAction('contacts/fetchContactRequest');
+export const fetchContactSuccess = createAction('contacts/fetchContactSuccsess');
+export const fetchContactError = createAction('contacts/fetchContactError');
+
+export const changeFilter=createAction('contacts/changeFilter')
